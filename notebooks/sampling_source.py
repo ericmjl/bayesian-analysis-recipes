7093cd81c2bc84b6a52d80bea810ed546eb7bdbb
Eric Ma <ericmajinglong@gmail.com>
2019-12-03 11:20:49 -0500

    Updated sampling log-likelihood notebook
    It is now an executable mirror of the original essay.
    Some changes do need to be mirrored in though.
    That is a task for another day.

---
+++ notebooks/sampling_source.py
@@ -0,0 +1,32 @@
+import numpy as np
+from scipy.stats import norm, expon
+
+
+class Distribution:
+    def __init__(self):
+        return NotImplementedError("Please implement init.")
+
+    def pdf(self, x):
+        """Evaluate total probability of data x."""
+        return np.sum(self.dist.pdf(x))
+
+    def logpdf(self, x):
+        """Evaluate total log probability of data x."""
+        return np.sum(self.dist.logpdf(x))
+
+    def draws(self, n):
+        """Draw n samples from the distribution"""
+        return self.dist.rvs(n)
+
+
+class Normal(Distribution):
+    def __init__(self, mu, sigma):
+        self.mu = mu
+        self.sigma = sigma
+        self.dist = norm(mu, sigma)
+
+
+class Exponential(Distribution):
+    def __init__(self, lam):
+        self.lam = lam
+        self.dist = expon(scale=lam)
